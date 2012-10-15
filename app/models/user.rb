cb7bbbb421af874596eefa764723be53ceaf8322
Andreas Nauleau <anauleau@gmail.com>
2012-10-15 19:23:13 -0400

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
 
-  before_save { |user| user.email = email.downcase }
-  before_save :create_remember_token
+  before_save { |user| user.email = user.email.downcase }
+  before_save :create_remember_token 
 
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -11,7 +12,12 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
-  
+
+  def feed
+    # This is only a proto-feed.
+    Micropost.where("user_id = ?", id)
+  end
+
   private
 
     def create_remember_token
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,7 +5,11 @@ describe "User pages" do
   subject { page }
 
   describe "index" do
+
     let(:user) { FactoryGirl.create(:user) }
+
+    before(:all) { 30.times { FactoryGirl.create(:user) } }
+    after(:all)  { User.delete_all }
 
     before(:each) do
       sign_in user
@@ -16,20 +20,19 @@ describe "User pages" do
     it { should have_selector('h1',    text: 'All users') }
 
     describe "pagination" do
-      before(:all) { 30.times { FactoryGirl.create(:user) } }
-      after(:all)  { User.delete_all }
-
       it { should have_selector('div.pagination') }
+
       it "should list each user" do
         User.paginate(page: 1).each do |user|
-          page.should have_selector('li', text: user.name)
+          page.should have_selector('li>a', text: user.name)
         end
-      end
+      end      
     end
+
     describe "delete links" do
-      
+
       it { should_not have_link('delete') }
-      
+
       describe "as an admin user" do
         let(:admin) { FactoryGirl.create(:admin) }
         before do
@@ -55,11 +58,19 @@ describe "User pages" do
 
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
 
     before { visit user_path(user) }
 
     it { should have_selector('h1',    text: user.name) }
-    it { should have_selector('title', text: user.name) }    
+    it { should have_selector('title', text: user.name) }  
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
+    end  
   end
 
   describe "signup" do
@@ -106,6 +117,7 @@ describe "User pages" do
       end
     end
   end
+
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
     before do
@@ -114,6 +126,7 @@ describe "User pages" do
     end
 
     describe "page" do
+
       it { should have_selector('h1',    text: "Update your profile") }
       it { should have_selector('title', text: "Edit user") }
       it { should have_link('change', href: 'http://gravatar.com/emails') }
@@ -124,9 +137,9 @@ describe "User pages" do
 
       it { should have_content('error') }
     end
-    
+
     describe "with valid information" do
-      let(:new_name)  { "New Name" }
+      let(:new_name) { "New Name" }
       let(:new_email) { "new@example.com" }
       before do
         fill_in "Name",             with: new_name
@@ -137,8 +150,8 @@ describe "User pages" do
       end
 
       it { should have_selector('title', text: new_name) }
-      it { should have_selector('div.alert.alert-success') }
       it { should have_link('Sign out', href: signout_path) }
+      it { should have_selector('div.alert.alert-success') }
       specify { user.reload.name.should  == new_name }
       specify { user.reload.email.should == new_email }
     end
